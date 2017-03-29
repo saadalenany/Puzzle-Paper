04c519679490b43cfccb177dff6c2d7ebceeacd1
saadalenany <saad.alenany@yahoo.com>
2017-03-29 20:57:11 +0200

    modified Checker

--- app/src/main/java/com/example/saad/puzzle_paper/Checker.java
+++ app/src/main/java/com/example/saad/puzzle_paper/Checker.java
@@ -1,14 +1,6 @@
 package com.example.saad.puzzle_paper;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.CountDownTimer;
 import android.os.Handler;
-import android.widget.Toast;
-
-import java.util.TimerTask;
 
 /**
  * Created by saad on 26/03/17.
@@ -16,21 +8,10 @@ import java.util.TimerTask;
 
 public class Checker{
 
-    Context con;
-
-    public Checker(final Integer firstImage, final int firstpos, final int position, final int rand, Context con) {
-        this.con = con;
+    public Checker(final int firstImage, final int firstpos, final int position, final int secondImage){
         //if the 2 indexes are equal then they've same image
-        if (firstImage == rand) {
-
-            int score = 0;
-            //if it's a golden image
-            if(firstImage == 1){
-                score = Integer.parseInt((String) MainActivity.val1.getText()) + 50;
-                Toast.makeText(con,"WOW it's a golden dice!",Toast.LENGTH_SHORT).show();
-            }else{
-                score = Integer.parseInt((String) MainActivity.val1.getText()) + 10;
-            }
+        if (firstImage == secondImage) {
+            int score = Integer.parseInt((String) MainActivity.val1.getText()) + 10;
             MainActivity.val1.setText("" + score);
             final Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
@@ -38,18 +19,9 @@ public class Checker{
                 public void run() {
                     MainActivity.imageViews[position].setImageResource(android.R.color.transparent);
                     MainActivity.imageViews[firstpos].setImageResource(android.R.color.transparent);
-                    MainActivity.randomimages.remove(rand);
                 }
-            }, 1000);
+            }, 700);
         } else {
-
-            if(Integer.parseInt((String) MainActivity.val1.getText()) <=0){
-                //keep it at 0
-            }else {
-                int score = Integer.parseInt((String) MainActivity.val1.getText()) - 10;
-                MainActivity.val1.setText("" + score);
-            }
-
             MainActivity.boolimgs[position] = false;
             MainActivity.boolimgs[firstpos] = false;
 
@@ -62,6 +34,7 @@ public class Checker{
                     MainActivity.imageViews[firstpos].setImageResource(R.drawable.cover);
                 }
             },700);
+            MainActivity.val2.setText("" + (Integer.parseInt((String) MainActivity.val2.getText()) + 1));
         }
     }
 }
